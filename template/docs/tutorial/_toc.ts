317b8631c6abe87f8cb3c8ed0c7c28f8f637f9c7
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2026-02-08 19:07:23 +0200

    add theme editor

--- template/docs/tutorial/_toc.ts
+++ template/docs/tutorial/_toc.ts
@@ -2,6 +2,7 @@ export default [
   { path: 'intro', label: 'Introduction', header: 'Users' },
   { path: 'docs', label: 'Writing Docs' },
   { path: 'deploying', label: 'Deploying' },
+  { path: 'theming', label: 'Theming' },
   { path: 'ui-elements/', label: 'UI Elements' },
   { path: 'overview', label: 'Overview', header: 'Contributors' },
   { path: 'cli-internals/', label: 'CLI Internals' },
