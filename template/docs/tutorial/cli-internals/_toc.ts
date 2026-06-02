6c15fbcd72533e2f1fada031f4e692525d25f2a8
Javi Rodriguez <3051496+lifeBalance@users.noreply.github.com>
2026-06-02 17:58:50 +0300

    document upgrade-template feature

--- template/docs/tutorial/cli-internals/_toc.ts
+++ template/docs/tutorial/cli-internals/_toc.ts
@@ -1,6 +1,7 @@
 export default [
   { path: 'intro', label: 'Introduction' },
   { path: 'deploy', label: 'Deploy Command' },
+  { path: 'upgrade-template', label: 'Upgrade Template' },
   { path: 'local-development', label: 'Local Development' },
   { path: 'how-it-works', label: 'How It Works' },
   { path: 'api/', label: 'API Reference' },
